e5d9631ed700bc9f33bacb268384714e026bdad7
987456987 <donufrak112697@gmail.com>
2023-09-21 22:25:51 -0700

    input state and corrected button classname logic

--- tip-calculator/src/components/Inputs.jsx
+++ tip-calculator/src/components/Inputs.jsx
@@ -1,6 +1,18 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react'
 
 function Inputs({ percent, setPercent }) {
+  const [percentInput, setPercentInput] = useState("")
+
+  const inputChange = (value) => {
+    setPercentInput(value)
+    setPercent(value)
+  }
+
+  const buttonClicked = (value) => {
+    setPercentInput("")
+    setPercent(value)
+  }
 
   return (
     <div className="container">
@@ -11,12 +23,12 @@ function Inputs({ percent, setPercent }) {
       <div className="input-container">
         <label>Select Tip %</label>
         <div className="button-group">
-          <button className={percent == 5 && "button-active"} onClick={() => setPercent(5)}>5%</button>
-          <button className={percent == 10 && "button-active"} onClick={() => setPercent(10)}>10%</button>
-          <button className={percent == 15 && "button-active"} onClick={() => setPercent(15)}>15%</button>
-          <button className={percent == 25 && "button-active"} onClick={() => setPercent(25)}>25%</button>
-          <button className={percent == 50 && "button-active"} onClick={() => setPercent(50)}>50%</button>
-          <input placeholder="Custom" onChange={(e) => setPercent(e.value)}/>
+          <button className={percent == 5 ? "button-active" : ""} onClick={() => buttonClicked(5)}>5%</button>
+          <button className={percent == 10 ? "button-active" : ""} onClick={() => buttonClicked(10)}>10%</button>
+          <button className={percent == 15 ? "button-active" : ""} onClick={() => buttonClicked(15)}>15%</button>
+          <button className={percent == 25 ? "button-active" : ""} onClick={() => buttonClicked(25)}>25%</button>
+          <button className={percent == 50 ? "button-active" : ""} onClick={() => buttonClicked(50)}>50%</button>
+          <input placeholder="Custom" onChange={(e) => inputChange(e.value)} value={percentInput}/>
         </div>
       </div>
       <div className="input-container">
