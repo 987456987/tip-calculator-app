b21a63bcf46ff8c669d2ca1938794fa35125bfd1
987456987 <donufrak112697@gmail.com>
2023-09-27 00:56:41 -0700

    display

--- tip-calculator/src/components/Display.jsx
+++ tip-calculator/src/components/Display.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
 function Display({ tipPercent, numPeople, billTotal }) {
-  const tipAmount = (parseFloat(billTotal) * (tipPercent/100))
-  const total = parseFloat(billTotal) + tipAmount
-  
+  const tipAmount = parseFloat(billTotal) * (tipPercent / 100);
+  const total = parseFloat(billTotal) + tipAmount;
+
   return (
     <div className="display-container">
       <div className="outer-container">
@@ -11,13 +11,19 @@ function Display({ tipPercent, numPeople, billTotal }) {
             <h3>Tip Amount</h3>
             <p className="extra">/ person</p>
           </div>
-          <div className="output">{tipAmount ? ((tipAmount/numPeople).toFixed(2).toString()) : "0.00"}</div>
+          <div className="output">
+            {tipAmount ? (tipAmount / numPeople).toFixed(2).toString() : "0.00"}
+          </div>
         </div>
         <div className="inner-container">
-          <h3>Total</h3>
-          <p className="extra">/ person</p>
+          <div className="inner-left-container">
+            <h3>Total</h3>
+            <p className="extra">/ person</p>
+          </div>
+          <div className="output">
+            {total ? (total / numPeople).toFixed(2).toString() : "0.00"}
+          </div>
         </div>
-          <div className="output">{total ? ((total/numPeople).toFixed(2).toString()) : "0.00"}</div>
       </div>
       <button>RESET</button>
     </div>
